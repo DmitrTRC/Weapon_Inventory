e575f0dd7c3b4eb987f4dcf78e726d44e9232480
Dmitry Morozov <9984398@gmail.com>
2023-07-01 19:43:41 +0300

    -> Start html section

--- Inventory/urls.py
+++ Inventory/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('weapon_items/', views.weapon_items, name='weapon_items'),
+
     # path('weapon_item/<int:weapon_item_id>/', views.weapon_item, name='weapon_item')
 ]
--- Inventory/views.py
+++ Inventory/views.py
@@ -1,14 +1,14 @@
 # import models
 from django.http import HttpResponse
 
+from .models import Weapon
 
 def index(request):
-    return HttpResponse("You're at the Inventory index.")
+
+    context = {
 
 
-def weapon_items(request):
-    return HttpResponse("You're at the weapon items.")
+    }
 
 
-def weapon_item(request, weapon_item_id):
-    return HttpResponse("You're looking at weapon item %s." % weapon_item_id)
+
