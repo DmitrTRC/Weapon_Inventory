32a7b8b1fb62d2609db2004f99826bc340bdc7a1
Dmitry Morozov <9984398@gmail.com>
2023-07-01 23:24:06 +0300

    -> Html update

--- Inventory/views.py
+++ Inventory/views.py
@@ -1,14 +1,12 @@
 # import models
 from django.http import HttpResponse
+from django.shortcuts import render
 
-from .models import Weapon
+from .models import WeaponItem
+
 
 def index(request):
+    weapons_list = WeaponItem.objects.order_by("item_license")
 
-    context = {
-
-
-    }
-
-
-
+    context = {"weapon_list": weapons_list}
+    return render(request, "index.html", context)
